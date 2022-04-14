ef52171710e560e5ee0ea2aeb387c306a8570b8f
@kumarnadi22 <kumarnadi22@gmail.com>
2022-04-14 19:13:18 +0530

    cor error

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const port = process.env.PORT || 8001
 const connection__url = "mongodb+srv://yatvik:yatvik07@cluster0.ybnwc.mongodb.net/url?retryWrites=true&w=majority"
 //middlewares
 app.use(express.json());
-app.use(cors(option));
+app.use(cors());
 let authenticate = (req, res, next) => {
     if (req.headers.authentication) {
         try {
